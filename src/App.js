0aed74dac4ed120985a8cbe128aa7a6389ce9c67
SheykoWk <sheyko.wk@gmail.com>
2022-04-01 21:21:36 -0600

    Add random background colors

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ function App() {
         return Math.floor(Math.random() * max.length - 1);
     };
 
+    const backColors = [
+      '#29294a', '#6b3051', '#835aa1', '#379b9c', '#a5c8c6'
+    ]
+
     // ? Initial State
     const [quote, setQuote] = useState(data.quotes[randomPosition(data.quotes)])
+    const [currentColor, setCurrentColor] = useState(backColors[randomPosition(backColors)])
 
     const handlerChangeQuote = () => {
       setQuote(data.quotes[randomPosition(data.quotes)])
+      setCurrentColor(backColors[randomPosition(backColors)])
     }
+
+
 
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" style={{backgroundColor : currentColor}} >
               <h3>{quote.quote}</h3>
               <p>{quote.author}</p>
               <button onClick={handlerChangeQuote} >Next Quote</button>
